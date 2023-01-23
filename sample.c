a707aec4f3ff9b2168635b6aa5da41abfb132d33
SravanthiAnnam401 <sannam@msystechnologies.com>
2023-01-23 12:22:43 +0530

    Sample additon program created

---
+++ sample.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+void add(int, int);
+
+int main()
+
+{
+int a, b;
+printf("Enter the two values to be added:");
+scanf("%d %d", &a, &b);
+
+add(a,b);
+return 0;
+
+}
+
+void add(int x, int y)
+{
+int sum;
+sum=x+y;
+printf("Sum is:%d\n",sum);
+
+}
